fcc5b19673de3c15eb04db3c0b3d27d4d0d45496
mkusaka <hinoshita1992@gmail.com>
2018-10-20 02:17:00 +0900

    change folders

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -5,4 +5,5 @@ module.exports = {
   base: '/docs/',
   serviceWorker: true,
   Markdown: true
+  // TODO: config sidebar https://vuepress.vuejs.org/theme/default-theme-config.html#disable-the-navbar
 }
