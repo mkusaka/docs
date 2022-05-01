f616fba3c2ff4a88fe79ef788d5941d07d85c9ff
mkusaka <hinoshita1992@gmail.com>
2022-05-02 01:06:12 +0900

    add workflow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const urlPrefix = process.env.URL_PREFIX ? "/" + process.env.URL_PREFIX : "";
+
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.js",
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
+  trailingSlash: true,
 });
 
 module.exports = withNextra();
