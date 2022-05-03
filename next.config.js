02f5e17cea7deeac0a6aa201a85f0f0d5e975aaf
mkusaka <hinoshita1992@gmail.com>
2022-05-03 14:16:42 +0900

    fix path

--- next.config.js
+++ next.config.js
@@ -3,9 +3,10 @@ const urlPrefix = process.env.URL_PREFIX ? "/" + process.env.URL_PREFIX : "";
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.js",
+});
+
+module.exports = withNextra({
   assetPrefix: urlPrefix,
   basePath: urlPrefix,
   trailingSlash: true,
 });
-
-module.exports = withNextra();
