48ddb7076606c819be586a5b5b7c4d44d48c288a
mkusaka <hinoshita1992@gmail.com>
2025-03-17 02:48:08 +0900

    use rspack

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,6 +6,9 @@ const darkCodeTheme = require("prism-react-renderer").themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
+  future: {
+    experimental_faster: true,
+  },
   title: "docs",
   tagline: "docs",
   url: "https://mkusaka.com",
