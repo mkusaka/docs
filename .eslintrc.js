bd48fd32377fe683b7d4d974a0e6f7dfe99c3108
mkusaka <hinoshita1992@gmail.com>
2026-08-19 02:51:05 +0900

    chore: migrate ESLint and Prettier config to oxlint and oxfmt
    - Replace `.eslintrc.js` with `.oxlintrc.json`
    - Replace `.prettierignore` with `.oxfmtrc.json`
    - Move lint/format ignore patterns into tool config files
    - Simplify package.json lint script

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ["next/core-web-vitals"],
-  rules: {
-    "react/jsx-curly-brace-presence": ["error", "never"],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "error",
-  },
-};
